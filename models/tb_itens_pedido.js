2fa53b1922f325f710df6c085f30a86c7a2ea6f1
Ragner Moura <44169840+ragnermoura@users.noreply.github.com>
2024-07-15 19:52:05 +0100

    fix: tb_itens_pedido_opcoes

--- models/tb_itens_pedido.js
+++ models/tb_itens_pedido.js
@@ -3,7 +3,6 @@ const conn = require("../data/conn");
 
 const Pedido = require("./tb_pedido");
 const Prato = require("./tb_pratos");
-const Opcao = require("./tb_opcoes");
 const Bebida = require("./tb_bebidas");
 
 const ItensPedido = conn.define(
@@ -57,38 +56,4 @@ ItensPedido.belongsTo(Bebida, {
   as: "bebida",
 });
 
-const ItensPedidoOpcoes = conn.define(
-  "tb_itens_pedido_opcoes",
-  {
-    id_item_pedido: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: ItensPedido,
-        key: 'id_item_pedido',
-      },
-    },
-    id_opcao: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: Opcao,
-        key: 'id_opcao',
-      },
-    },
-  },
-  { freezeTableName: true }
-);
-
-ItensPedido.belongsToMany(Opcao, {
-  through: ItensPedidoOpcoes,
-  as: 'opcoes',
-  foreignKey: 'id_item_pedido',
-});
-
-Opcao.belongsToMany(ItensPedido, {
-  through: ItensPedidoOpcoes,
-  as: 'itens_pedido',
-  foreignKey: 'id_opcao',
-});
-
 module.exports = ItensPedido;
-module.exports = ItensPedidoOpcoes;

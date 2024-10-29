45884a0fb0101379dcf95df37056ca615eefe0ff
andressaricardo-precisao <andressa@softwareprecisao.com>
2024-10-29 14:26:27 -0300

    feat: implementando o usuario na get do restaurante por id

--- controller/restauranteController.js
+++ controller/restauranteController.js
@@ -136,6 +136,9 @@ const restauranteController = {
             model: Avaliacao,
             as: "avaliacoes",
           },
+          {
+            model: Usuario,
+          },
         ],
       });
 

5745dad5a4d7bee3526df1c5d26b9b2404c911e4
andressaricardo-precisao <andressa@softwareprecisao.com>
2024-11-04 16:20:34 -0300

    feat: rotas de get pro pagamento

--- config/eupagoConfig.js
+++ config/eupagoConfig.js
@@ -1,14 +1,11 @@
-
-
-
 module.exports = {
-    clientId: '8a418e1ffae44794b329285e84ac2958',
-    clientSecret: '8229382cd50243bd877001e0b93dc74b',
-    environment: process.env.NODE_ENV || 'sandbox', // 'sandbox' ou 'production'
-    get apiUrl() {
-      return this.environment === 'production'
-        ? 'https://clientes.eupago.pt/api'
-        : 'https://sandbox.eupago.pt/api';
-    },
-  };
-  
+  clientId: process.env.API_KEY,
+  clientSecret: process.env.CLIENT_SECRET,
+  apiKey: process.env.API_KEY,
+  environment: process.env.NODE_ENV || "sandbox",
+  get apiUrl() {
+    return this.environment === "production"
+      ? process.env.URL_PROD
+      : process.env.URL_SANDBOX;
+  },
+};
--- controller/authController.js
+++ controller/authController.js
@@ -6,16 +6,23 @@ let authToken = null; // Token armazenado para uso nas requisições
 
 // Função para autenticar e obter o token usando Client ID e Client Secret
 const authenticate = async () => {
-  console.log(eupagoConfig.clientId, eupagoConfig.clientSecret);
+  console.log("Client ID:", eupagoConfig.clientId);
+  console.log("Client Secret:", eupagoConfig.clientSecret);
+
   try {
-    const response = await axios.post(`${eupagoConfig.apiUrl}/auth/token`, {
-      clientId: eupagoConfig.clientId,
-      clientSecret: eupagoConfig.clientSecret,
-
-
-      
-    });
+    const response = await axios.post(
+      `${eupagoConfig.apiUrl}/auth/token`,
+      null, // Adicione null para o corpo vazio
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Basic ${Buffer.from(`${eupagoConfig.clientId}:${eupagoConfig.clientSecret}`).toString('base64')}`,
+        },
+      }
+    );
+    
     authToken = response.data.token; // Armazena o token
+    console.log("Token recebido:", authToken); // Adicione esse log para verificar o token
     return authToken;
   } catch (error) {
     console.error('Erro ao autenticar com a Eupago:', error.message);

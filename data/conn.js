3630fdf687a617ebff89c3ad6e6a15eea6bca76e
andressaricardo-precisao <andressa@softwareprecisao.com>
2024-11-04 16:29:17 -0300

    feat: colocando o logging:false pra não mostrar o historico de queries no terminal

--- data/conn.js
+++ data/conn.js
@@ -3,7 +3,8 @@ require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    logging: false
 });
 try {
 

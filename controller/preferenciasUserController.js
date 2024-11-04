f6b8f8a2f31340b9c0ed225570513925ee3f1e8b
andressaricardo-precisao <andressa@softwareprecisao.com>
2024-11-04 11:22:43 -0300

    feat: criando as preferencias automaticamente quando cria o usuario

--- controller/preferenciasUserController.js
+++ controller/preferenciasUserController.js
@@ -63,6 +63,48 @@ const PreferenciasUsuarioController = {
         .json({ message: "Erro ao atualizar preferências." });
     }
   },
+
+  async criarPreferencias(req, res) {
+    const { id_user } = req.params;
+    const {
+      notificacoes_pedido = false,
+      notificacoes_dicas_e_promocao = false,
+      email_dicas_e_promocao = false,
+      notificacoes_ofertas_parceiros = false,
+      email_ofertas_parceiros = false,
+    } = req.body;
+
+    try {
+      const [preferencias, created] = await PreferenciasUsuario.findOrCreate({
+        where: { id_user },
+        defaults: {
+          notificacoes_pedido,
+          notificacoes_dicas_e_promocao,
+          email_dicas_e_promocao,
+          notificacoes_ofertas_parceiros,
+          email_ofertas_parceiros,
+        },
+      });
+
+      if (created) {
+        return res.status(201).json({
+          message: "Preferências criadas com sucesso.",
+          preferencias,
+        });
+      } else {
+        return res.status(200).json({
+          message: "Preferências já existentes para o usuário.",
+          preferencias,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        message: "Erro ao criar preferências.",
+      });
+    }
+}
+
 };
 
 module.exports = PreferenciasUsuarioController;

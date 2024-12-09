e39009636e0db8f39c745ffa4529e525e8da1208
andressaricardo-precisao <andressa@softwareprecisao.com>
2024-12-09 14:21:32 -0300

    feat: rota pra alterar status da cordenada

--- src/routes/lat_long.js
+++ src/routes/lat_long.js
@@ -1,20 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const latLongController = require('../controller/latlongController');
+const latLongController = require("../controller/latlongController");
 
-// Rota para buscar todas as coordenadas
-router.get('/', latLongController.buscarTodasCoordenadas);
+router.get("/", latLongController.buscarTodasCoordenadas);
 
-// Rota para buscar uma coordenada específica por ID
-router.get('/:id', latLongController.buscarCoordenadasPorId);
+router.get("/:id", latLongController.buscarCoordenadasPorId);
 
-// Rota para atualizar uma coordenada por ID
-router.put('/editar/:id', latLongController.atualizarCoordenada);
+router.put("/editar/:id", latLongController.atualizarCoordenada);
 
-// Rota para adicionar uma nova coordenada
-router.post('/cadastrar', latLongController.adicionarCoordenadas);
+router.post("/cadastrar", latLongController.adicionarCoordenadas);
 
-// Rota para remover uma coordenada por ID
-router.delete('/deletar/:id', latLongController.removerCoordenada);
+router.put("/edit/:id/status", latLongController.atualizarStatus);
+
+router.delete("/deletar/:id", latLongController.removerCoordenada);
 
 module.exports = router;

c51ddcf6daecc80ed2309e3e059c1a4c11195323
andressaricardo-precisao <andressa@softwareprecisao.com>
2025-02-28 13:07:56 -0300

    refactor: improve database connection handling and formatting

--- data/conn.js
+++ data/conn.js
@@ -1,18 +1,26 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config()
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false
-});
-try {
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
-    sequelize.authenticate()
-    console.log('Success connect');
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Success connect");
 
-} catch (error) {
-    console.error(error);
-}
+    await sequelize.sync();
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+  }
+})();
 
-module.exports = sequelize
+module.exports = sequelize;
